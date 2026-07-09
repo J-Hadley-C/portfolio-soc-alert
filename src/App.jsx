4086e6ec2101c9e81675b4397dd5da08cd059345
Jean-Hadley Chery <jhadleyc@mail.com>
2026-07-09 22:02:19 +0200

    feat: assembler toutes les sections avec la navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Profil from './components/Profil'
 import Competences from './components/Competences'
@@ -7,13 +8,20 @@ import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <>
-      <Hero />
-      <Profil />
-      <Competences />
-      <Projets />
-      <Contact />
+    <div className="min-h-screen bg-bg">
+      <Navbar />
+      <main>
+        <Hero />
+        <div className="divider-line" />
+        <Profil />
+        <div className="divider-line" />
+        <Competences />
+        <div className="divider-line" />
+        <Projets />
+        <div className="divider-line" />
+        <Contact />
+      </main>
       <Footer />
-    </>
+    </div>
   )
 }
